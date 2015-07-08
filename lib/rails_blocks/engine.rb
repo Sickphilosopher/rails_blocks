d36d60926f082b3f67ef191c489b0dfb010548b4
Andrei Potetiurin <sickphilosopher@gmail.com>
2015-07-08 19:43:20 +0300

    added precompile

--- lib/rails_blocks/engine.rb
+++ lib/rails_blocks/engine.rb
@@ -2,8 +2,12 @@ require 'rails_blocks/helpers/block_helper'
 
 module RailsBlocks
 	class Engine < Rails::Engine
-		initializer "rails_blocks.configure_rails_initialization" do
+		initializer 'rails_blocks.configure_rails_initialization' do
 			require 'rails_blocks/initializers/rails_blocks'
+		end
+		
+		initializer 'rails_blocks.precompile', :group => :all do |app|
+			app.config.assets.precompile += ['rails_blocks.coffee']
 		end
 		
 		ActiveSupport.on_load :action_view do
