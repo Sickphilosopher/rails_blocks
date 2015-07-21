8cfbadc1be33a8eb85e4041c13e1d20f5027e410
Andrei Potetiurin <sickphilosopher@gmail.com>
2015-07-21 22:32:08 +0300

    first working rails_blocks helper

--- app/helpers/block_helper.rb
+++ app/helpers/block_helper.rb
@@ -1,38 +1,110 @@
 module BlockHelper
 	include RailsBlocks::Path
-	include RailsBlocks::Blocks
+
 	def b(b_name, options = {}, &block)
+		template = find_template b_name, options
+		@blocks_stack = @blocks_stack || []
+		@blocks_stack.push(b_name)
 		
-		dir = b_name
-		filename = b_name
-		dir, filename = add_mods(dir, filename, options)
-		target = [dir, filename].join('/').gsub('-', '_')
-		# block = RailsBlocks.get_block b_name
-		# block.render.html_safe
-		#dir.to_s
+		if block_given?
+			@content = capture(&block)
+		end
 		
-		Slim::Template.new{ ".test\n	| test"}.render
-		template_exists?(target) ? render(file: target) : target.to_s
-		block = Block.new('')
-		block.render
+		@attrs = {}
+		@attrs[:class] = block_classes b_name, options
+		if template.nil?
+			result = empty
+		else
+			@attrs[:tag] = options[:tag] || 'div'
+			result = render(file: template)
+		end
+		@blocks_stack.pop
+		result
+	end
+	
+	def e(e_name, options = {}, &block)
+		@content = capture(&block)
+		@attrs = {class: element_classes(@blocks_stack.last, e_name)}
+		empty
 	end
 	
 	def empty
-		'test empty'
+		atrrs = @attrs.map {|key, value| key.to_s + '="' + value.to_s + "\""}.join(' ')
+		"<div #{atrrs}>#{content}</div>".html_safe
 	end
 	
-	def this
-		{tag: 'test', attrs: {class: 'test-class', boo: 'test-boo'}}
+	def bem_attrs
+		@attrs
+	end
+	
+	def content
+		@content
 	end
 	
 	private
-		def add_mods(dir, filename, options = {})
-			unless(options[:mods].nil?)
-				mod, value = options[:mods].first
-				mod_s = "_#{mod.to_s}_#{value.to_s}"
-				dir = File.join dir, mod_s
-				filename += mod_s
-			end
+	
+		def element_classes(b_name, e_name)
+			classes = [element_class(b_name, e_name)]
+			classes.join(' ')
+		end
+		
+		def block_classes(b_name, options)
+			base_class = block_class(b_name)
+			classes = [base_class]
+			classes << mods_classes(base_class, options[:mods]) unless options[:mods].nil?
+			classes << mix_class(options[:mix]) unless options[:mix].nil?
+			classes.join(' ')
+		end
+	
+		def add_mods(block_dir, b_name, mods)
+			mod, value = mods.first
+			mod_s = "_#{mod.to_s}_#{value.to_s}"
+			dir = File.join block_dir, mod_s
+			filename = b_name + mod_s
 			return dir, filename
+		end
+		
+		def mods_classes(base_class, mods)
+			mods.map {|key, value| base_class + RailsBlocks.config.modifier_separator + key.to_s + '_' + value.to_s}
+		end
+		
+		def mix_class(mix)
+			element_class(mix[:b], mix[:e])
+		end
+		
+		def block_class(b_name)
+			RailsBlocks.config.prefix + b_name
+		end
+		
+		def element_class(b_name, e_name)
+			block_class(b_name) + RailsBlocks.config.element_separator + e_name
+		end
+		
+		def find_template(b_name, options = {})
+			block_dir = get_block_dir b_name
+			return nil if block_dir.nil?
+			
+			unless options[:mods].nil?
+				block_dir_with_mod, filename = add_mods(block_dir, b_name, options[:mods])
+				path = File.join block_dir_with_mod, filename + RailsBlocks.config.template_engine
+				if File.exists? path
+					return path
+				end
+			end
+			filename = b_name
+			path = File.join block_dir, filename + RailsBlocks.config.template_engine
+			if File.exists? path
+				return path
+			end
+		end
+		
+		def get_block_dir(b_name)
+			RailsBlocks.config.levels.reverse.each do |level|
+				block_dir = File.join(blocks_dir, level, b_name)
+				if Dir.exists? block_dir
+					return block_dir
+				end
+			end
+			nil
 		end
 end
--- lib/rails_blocks/configuration.rb
+++ lib/rails_blocks/configuration.rb
@@ -3,11 +3,17 @@ module RailsBlocks
 		attr_accessor :prefix
 		attr_accessor :blocks_dir
 		attr_accessor :levels
+		attr_accessor :template_engine
+		attr_accessor :element_separator
+		attr_accessor :modifier_separator
 		
 		def initialize
 			@prefix = 'b-'
 			@blocks_dir = 'app/blocks'
 			@levels = []
+			@template_engine = '.slim'
+			@element_separator = '__'
+			@modifier_separator = '--'
 		end
 	end
 end
